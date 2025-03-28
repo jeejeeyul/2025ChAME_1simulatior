8897e221f1bcc1b9b4982de4e68d642039ce196f
jeejeeyul <jee200713@gmail.com>
2025-03-28 22:35:51 +0900

    타일 찍음

---
+++ Assets/moving.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 moveVelocity = new Vector3(x, y, 0) * 5 * Time.deltaTime;
+        this.transform.position += moveVelocity;
+    }
+}

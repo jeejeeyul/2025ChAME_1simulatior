ede30ba15312d3d63db705d71ed2cd728066ebb0
jeejeeyul <jee200713@gmail.com>
2025-06-20 16:07:40 +0900

    대화 띄우기

---
+++ Assets/DialogueData.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class DialogueData {
+    public int id;
+    public string characterName;
+    public string dialogue;
+    public string backgroundName;
+    public string choice1Text;
+    public int choice1NextID;
+    public string choice2Text;
+    public int choice2NextID;
+}
